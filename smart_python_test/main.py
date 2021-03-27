a7032cf46179c980313bb84a124f00c5f0046162
Nadia Ali <nadiaagnesali@gmail.com>
2021-03-27 07:36:15 +0000

    remove file

--- smart_python_test/main.py
+++
@@ -1,9 +0,0 @@
-# Query API
-import requests
-# import json
-response = requests.get('https://api.opencorporates.com/v0.4/companies/secarch?q=smart')
-print(response)
-
-# # Get list of companies with word "smart" in their name
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
