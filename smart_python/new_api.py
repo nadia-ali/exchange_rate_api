ee4d41760afbce169fff76d71b16554025a861a1
Nadia Ali <nadiaagnesali@gmail.com>
2021-03-27 20:29:53 +0000

    updated

--- smart_python/new_api.py
+++ smart_python/new_api.py
@@ -1,14 +1,19 @@
+"""base file. once complete, make changes to main file, then remove other folders"""
 # 3rd party
 import requests
+import pandas as pd
+
 # in-built
 import json
+import sqlite3
+from sqlite3 import Error
+# import csv
 
-api_url = 'https://api.exchangeratesapi.io/latest'
-response = requests.get(api_url)
 
-
-def handling_error_codes(r):
+def handling_error_codes():
     # Error handling if the API returns an error
+    api_url = 'https://api.exchangeratesapi.io/latest'
+    r = requests.get(api_url)
     if r.status_code == 200:
         print('Success!')
     elif r.status_code == 403:
@@ -19,37 +24,85 @@ def handling_error_codes(r):
         print('Error, response code is: {}'.format(r.status_code))
 
 
-def query_api():
-    # Querying the api for exchange rates for the period 01/01/2018 - 01/01/2021
-    exchange_rates = requests.get('https://api.exchangeratesapi.io/'
-                                  'history?start_at=2018-01-01&end_at=2021-01-01&base=DKK')
-    data = exchange_rates.text
+def get_rates():
+    # Querying the api for specific exchange rates for the period 01/01/2018 - 01/01/2021
+    rates = requests.get("https://api.exchangeratesapi.io/"
+                         "history?start_at=2018-01-01&end_at=2021-01-01&symbols=GBP,USD,EUR&base=DKK")
+    data = rates.text
     parsed = json.loads(data)
-    return parsed
+    parsed_rates = parsed["rates"]
+    return parsed_rates
 
 
-def exchange_currency():
-    # Extract exchange and conversion rates
-    parsed_data = query_api()
-    rates = parsed_data["rates"]
-    for key, val in rates.items():
-        print("Date: ", key, "GBP Rate: ", val['GBP'])
-        print("Date: ", key, "USD Rate: ", val['USD'])
-        print("Date: ", key, "EUR Rate: ", val['EUR'])
-
-    # Extract conversion rates to all currencies
-
-
-# def output_to_csv():
+def output_to_csv():
     # Writing data to CSV file
+    csv_data = get_rates()
+    data_frame = pd.DataFrame(csv_data)
+    csv_file = data_frame.to_csv('./rates.csv')
+    return csv_file
 
 
-# def csv_to_db():
+def db_connection(db_file):
+    """ create a database connection to the SQLite database
+        :param db_file: database file
+        :return: Connection object or None """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def csv_to_db(db_file):
     # Upload CSV to database
+    connection = db_connection(db_file)
+    cur = connection.cursor()
+    cur.execute('''CREATE TABLE rates
+                   (date text, currency text, rate text)''')
+    connection.commit()
+    connection.close()
+
+
+def create_table(create_table_sql=None):
+    # create database table which will store data
+    try:
+        c = db_connection.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+# 
+# def insert_data():
+#     Insert csv file data to table within database
+
+# def query_api():
+#
+#     exchange_rates = requests.get('https://api.exchangeratesapi.io/'
+#                                   'history?start_at=2018-01-01&end_at=2021-01-01&base=DKK')
+#     data = exchange_rates.text
+#     parsed = json.loads(data)
+#     return parsed
+
+
+# def exchange_currency():
+#     # Extract exchange and conversion rates
+#     parsed_data = query_api()
+#     rates = parsed_data["rates"]
+#     data = ""
+#     for key, val in rates.items():
+#         print("Date: ", key, "GBP Rate: ", val['GBP'])
+#         print("Date: ", key, "USD Rate: ", val['USD'])
+#         print("Date: ", key, "EUR Rate: ", val['EUR'])
 
 
 if __name__ == "__main__":
-    handling_error_codes(response)
-    query_api()
-    exchange_currency()
-    # conversion_rates()
+    handling_error_codes()
+    # query_api()
+    # exchange_currency()
+    get_rates()
+    output_to_csv()
+    db_connection("")
+    csv_to_db("")
+
