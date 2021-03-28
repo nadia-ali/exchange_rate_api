13c20b2b67356812ee0a76379fa2752cd02aad54
Nadia Ali <nadiaagnesali@gmail.com>
2021-03-28 16:12:05 +0100

    added unit tests

--- smart_python/tests.py
+++ smart_python/tests.py
@@ -1,1 +1,19 @@
-# Automated unit tests
+# in-built
+import unittest
+# created file
+from new_api import get_rates
+from new_api import output_to_csv
+
+
+class TestRates(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertDictEqual(d1=get_rates(), d2=get_rates())
+
+    def test_csv(self):
+        self.assertIsNotNone(output_to_csv())
+
+
+if __name__ == '__main__':
+    unittest.main()
+    print("Tests have passed")
